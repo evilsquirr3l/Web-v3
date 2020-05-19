c1cb8b70b8ea67bee5024994fca42571976038ca
Serhii Denshchyk <serhii_denshchyk@epam.com>
2020-05-19 19:27:58 +0300

    Model fix

--- Data.Entities/Apartment.cs
+++ Data.Entities/Apartment.cs
@@ -5,6 +5,11 @@ namespace Data.Entities
     public class Apartment : BaseEntity
     {
         public string Type { get; set; }
+
+        public int HouseId { get; set; }
+
+        public House House { get; set; }
+        
         public ICollection<ApartmentResidents> Residents { get; set; }
     }
 }
