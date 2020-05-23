ddea4e535f05849516f5c99be700da709fe38c46
Serhii Denshchyk <serhii_denshchyk@epam.com>
2020-05-23 22:04:33 +0300

    Configure identity options

--- Data.Implementation/DataServices.cs
+++ Data.Implementation/DataServices.cs
@@ -1,5 +1,7 @@
+using System;
 using Data.Abstraction;
 using Data.Entities;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -12,15 +14,32 @@ namespace Data.Implementation
             services.AddDbContext<MapDbContext>(options =>
                 options.UseSqlServer(connectionString));
 
-            services.AddIdentityCore<User>().AddEntityFrameworkStores<MapDbContext>();
+            services.AddIdentityCore<User>()
+                .AddEntityFrameworkStores<MapDbContext>()
+                .AddSignInManager();
 
             services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
-            
+
             services.AddScoped<IResidentRepository, ResidentRepository>();
-            
+
             services.AddScoped<IApartmentRepository, ApartmentRepository>();
 
             services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+            services.Configure<IdentityOptions>(options =>
+            {
+                options.Password.RequiredLength = 6;
+
+                // Lockout settings.
+                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+                options.Lockout.MaxFailedAccessAttempts = 5;
+                options.Lockout.AllowedForNewUsers = true;
+
+                // User settings.
+                options.User.AllowedUserNameCharacters =
+                    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
+                options.User.RequireUniqueEmail = true;
+            });
 
             return services;
         }
