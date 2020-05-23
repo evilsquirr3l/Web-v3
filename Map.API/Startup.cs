7870e4582152deaaf4142850f250ce81047b3e38
Serhii Denshchyk <serhii_denshchyk@epam.com>
2020-05-23 22:04:52 +0300

    Add identity

--- Map.API/Startup.cs
+++ Map.API/Startup.cs
@@ -1,10 +1,16 @@
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
 using Business.Implementation;
+using Business.Implementation.Configurations;
 using Data.Implementation;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 
 namespace Map.API
@@ -29,10 +35,35 @@ namespace Map.API
 
             services.RegisterBusinessServices();
             
+            services.Configure<JwtSettings>(Configuration.GetSection("JWTSettings"));
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo {Title = "Map", Version = "v1"});
             });
+            
+            JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
+            
+            services
+                .AddAuthentication(options =>
+                {
+                    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+                    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                    
+                })
+                .AddJwtBearer(cfg =>
+                {
+                    cfg.RequireHttpsMetadata = false;
+                    cfg.SaveToken = true;
+                    cfg.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidIssuer = Configuration["JWTSettings:Issuer"],
+                        ValidAudience = Configuration["JWTSettings:Audience"],
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["JWTSettings:SecretKey"])),
+                        ClockSkew = TimeSpan.Zero
+                    };
+                });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
